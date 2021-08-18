daad5e5570e7ba7998f224d85ce64e53e0e6f1c6
Muhammad Arsalan <arsalan@Muhammads-MacBook-Pro.local>
2021-08-19 01:11:23 +0500

    Header Completed

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwI90REa3M_HveSgjaa1G37jkEtD1Q6Uc",
+    authDomain: "clone-d6a48.firebaseapp.com",
+    projectId: "clone-d6a48",
+    storageBucket: "clone-d6a48.appspot.com",
+    messagingSenderId: "170524981139",
+    appId: "1:170524981139:web:8debcd0b5fdc6426679a66"
+  };
